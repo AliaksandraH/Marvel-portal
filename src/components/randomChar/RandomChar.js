d7d215e35ff23d60668421c9ea4dcce6cf4cda10
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2023-04-23 13:46:36 +0200

    Error Boundaries and componentDidUpdate

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,11 +7,6 @@ import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
-
     state = {
         char: {},
         loading: true,
@@ -19,6 +14,15 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 10000);
+    }
+
+    componentWillUnmount() {
+        // clearInterval(this.timerId);
+    }
 
     onCharLoaded = (char) => {
         this.setState({char, loading: false})
@@ -29,8 +33,11 @@ class RandomChar extends Component {
     }
 
     updateChar = () => {
+        this.setState({char: {}, loading: true, error: false })
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService.getCharacter(id).then(this.onCharLoaded).catch(this.onError);
+
+        console.log('updateChar');
     }
 
     render() {
@@ -52,7 +59,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -65,10 +72,14 @@ class RandomChar extends Component {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
     const desc = !description ? 'There is no description for this character' : description.slice(0, 170) + '...';
+    let classForImg = {objectFit: 'cover'};
+    if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'){
+        classForImg = {objectFit: 'contain'};
+    }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={classForImg}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">{desc}</p>
