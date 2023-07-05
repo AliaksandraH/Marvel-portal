55af54d5713df9685269c44a4e0b43efdb10bfc0
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2023-07-05 12:58:17 +0200

    New page ComicsList

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-// import MarvelService from "./services/MarvelService";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
 
-import './style/style.scss';
+import "./style/style.scss";
 
-// const marvelService = new MarvelService();
-// marvelService.getAllCharacters().then(res => console.log(res));
-// marvelService.getCharacter(1011196).then(res => console.log(res));
-// marvelService.getAllCharacters().then(res => res.data.results.forEach(item=>console.log(item.name)));
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
