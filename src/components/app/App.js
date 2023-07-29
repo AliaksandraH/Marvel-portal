64dc20c81c52fbe3b3e6868b7ca75aa626616e17
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2023-07-29 13:17:48 +0300

    Find a character by name

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,10 +5,13 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Spinner from "../spinner/Spinner";
 import AppHeader from "../appHeader/AppHeader";
 
+import SingleCharacterLayout from "../pages/singleCharacterLayout/SingleCharacterLayout";
+import SingleComicLayout from "../pages/singleComicLayout/SingleComicLayout";
+import SinglePage from "../pages/SinglePage";
+
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
 
 const App = () => {
     return (
@@ -21,8 +24,22 @@ const App = () => {
                             <Route path="/" element={<MainPage />} />
                             <Route path="/comics" element={<ComicsPage />} />
                             <Route
-                                path="/comics/:comicId"
-                                element={<SingleComicPage />}
+                                path="/comics/:id"
+                                element={
+                                    <SinglePage
+                                        Component={SingleComicLayout}
+                                        dataType="comic"
+                                    />
+                                }
+                            />
+                            <Route
+                                path="/characters/:id"
+                                element={
+                                    <SinglePage
+                                        Component={SingleCharacterLayout}
+                                        dataType="character"
+                                    />
+                                }
                             />
                             <Route path="*" element={<Page404 />} />
                         </Routes>
